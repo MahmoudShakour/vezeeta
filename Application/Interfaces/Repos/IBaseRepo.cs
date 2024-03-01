e47fb16ef467d948b8da5e7b79f070859320b87a
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2024-03-01 14:57:14 +0200

    Add base repo

---
+++ Application/Interfaces/Repos/IBaseRepo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repos
+{
+    public interface IBaseRepo<T,TId>
+    {
+        Task<T?> GetById(TId id);
+        Task<List<T>> GetAll();
+        void Delete(T entity);
+        Task<T> Create(T entity);
+        Task<List<T>> FindAll(Expression<Func<T, bool>> predicate);
+        Task<T?> FindOne(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ Infrastructure/Repos/BaseRepo.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Application.Interfaces.Repos;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repos
+{
+    public class BaseRepo<T, TId> : IBaseRepo<T, TId> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> Create(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<List<T>> FindAll(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<T?> FindOne(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public Task<List<T>> GetAll()
+        {
+            return _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetById(TId id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+    }
+}
